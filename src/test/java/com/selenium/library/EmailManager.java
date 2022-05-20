ced3140e60ec2561433680b6c50f0e35f19914f9
SamianTest <samina_er@yahoo.com>
2022-05-20 15:34:58 -0400

    super final

--- src/test/java/com/selenium/library/EmailManager.java
+++ src/test/java/com/selenium/library/EmailManager.java
@@ -4,7 +4,7 @@ public class EmailManager {
 	
 	
 	public void iAmHere() {
-		System.out.println("I am new develop tool");
+		System.out.println("I am new develop tool, goooooooo");
 		System.out.println("Good to go for merge");
 		
 	}
